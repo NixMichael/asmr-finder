398ccafef224d8699a03d013f6ced52d182e34ad
Michael Nix <mpnix@protonmail.com>
2021-09-07 18:56:59 +0100

    Add styling to results

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends Controller
     public function index ()
     {
         $response = Http::get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCE6acMV3m35znLcf0JGNn7Q&key=".env('API_KEY')."&maxResults=15");
-
         $results = json_decode($response);
 
         return view('home', [
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,19 @@
 <x-layout>
-    <div class="w-5/6 mx-auto flex flex-wrap justify-around">
-    @foreach ($content as $video)
-        @if (isset($video->id->videoId))
-            <article class="w-5/12 flex flex-col items-center my-3 bg-gray-600 p-3 rounded-md">
-                <h4 class="mb-4">{{ $video->snippet->title }}</h4>
-                <a href="https://youtube.com/watch?v={{ $video->id->videoId ?? 'ha' }}">
-                    <img src="{{ $video->snippet->thumbnails->high->url }}" alt="{{ $video->snippet->title }}">
-                </a>
-            </article>
-        @endif
-    @endforeach
+    <div class="w-11/12 lg:w-5/6 mx-auto flex flex-col lg:flex-row flex-wrap justify-around">
+    @if (is_array($content))
+        @foreach ($content as $video)
+            @if (isset($video->id->videoId))
+                <article class="lg:w-5/12 flex flex-col items-center my-3 bg-gray-600 p-3 rounded-md">
+                    <h4 class="mb-4 h-12">{!! $video->snippet->title !!}</h4>
+                    <a href="https://youtube.com/watch?v={{ $video->id->videoId }}">
+                        <div class="w-full relative" style="padding-top: 52.25%">
+                            <div class="absolute w-full h-full bg-no-repeat bg-center" style=" margin-top: -52.25%; background-image: url('{{ $video->snippet->thumbnails->high->url }}')">
+                        </div>
+                    </a>
+                </article>
+            @endif
+        @endforeach
+    @else
+        <p>Failed</p>
+    @endif
 </x-layout>
